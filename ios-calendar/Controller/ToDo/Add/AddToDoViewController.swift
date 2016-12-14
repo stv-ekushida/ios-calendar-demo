ab752a8e5e5995933872f96a9223ee83b15cffab
stv-ekushida <e.kushida@st-ventures.jp>
2016-12-15 00:21:03 +0900

    Update ToDoの登録処理を追加

--- ios-calendar/Controller/ToDo/Add/AddToDoViewController.swift
+++ ios-calendar/Controller/ToDo/Add/AddToDoViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 final class AddToDoViewController: UIViewController {
 
+    @IBOutlet weak var navigationBar: UINavigationBar!
     static var storyboardName: String {
         get {
             return String(describing: self)
@@ -21,15 +22,45 @@ final class AddToDoViewController: UIViewController {
             return String(describing: self)
         }
     }
+    var dateString = ""
 
     //MARK:-LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateTitle()
+        loadTodos()
     }
 
     //MARK:-Actions
-    @IBAction func didTapCancel(_ sender: UIBarButtonItem) {
+    @IBAction func didTapAdd(_ sender: UIBarButtonItem) {
+        addToDo()
+    }
+        
+    @IBAction func didTapBack(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    fileprivate func updateTitle() {
+        
+        navigationBar.topItem?.title = dateString.str2DateString(separated: .month) + "月"
+            + dateString.str2DateString(separated: .day) + "日"
+    }
+    
+    fileprivate func loadTodos() {
+        
+        let todos = ToDoUsecase.findTodoAt(yyyymd: dateString)
+        
+        _ = todos.map {
+            print($0.title, $0.targetDate?.description)
+        }
+    }
+    
+    fileprivate func addToDo() {
+        
+        var todo = ToDoEntity()
+        todo.title = "test"
+        todo.targetDate = dateString.str2Date(format: "yyyy-MM-dd")
+        _ = ToDoUsecase.addTodo(todo: todo)
     }
 }
 
